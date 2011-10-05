0f74b1cec0d8493c66a159a7a194fd29e11bb745
Cory Hagerman <chagerman08@gmail.com>
2011-10-05 14:48:46 -0400

    Continued modifying parent.c

--- parent.c
+++ parent.c
@@ -4,13 +4,17 @@
 #include <signal.h>
 #include <time.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void child (int, char * argv[]);
 
 int main(int argc, char * argv[])
 {
  // char* child1[MAX_ARGS];
  // char* child2[MAX_ARGS];
     pid_t pid;
-    int child_status;
+    int status;
     int argc_temp = argc;
  // printf("argc %d\n", argc_temp);
     if (argc_temp <= 2){
@@ -24,20 +28,25 @@ int main(int argc, char * argv[])
     }
     else if (!pid)//child
     {
+	child(argc_temp/2 + 1, argv);
+/*
 	printf("Child %d is now running\n", getpid());
 	char ** parmList2;
 	parmList2 = (char **)calloc(argc_temp/2 + 2, sizeof(char*));
         //char * parmList2[argc_temp/2 + 2];// = {argv[0]};
 	strcpy(parmList2[0],argv[0]);
-	for (int i = 1; i < argc_temp/2 + 1; i ++)
+	for (int i = 1; i < argc_temp/2 + 1; i ++){
+	    parmList2[i] = malloc(sizeof(argv[i]));
 	    strcpy(parmList2[i],argv[i]);
 	    //parmList2[i] = argv[i];
+	}
 	parmList2[argc_temp/2 + 1] = NULL;
 	printf("im a child :%s: :%s:\n", argv[0], argv[1]);	
 	printf("%d\n",(argc_temp/2 + 1));
         //exit(0);
         sleep(1);
 	execvp(parmList2[0], parmList2);
+*/
     }
     else{ //parent
     /*    char * parmList3[(argc_temp - argc_temp/2)];// = {argv[0]};
@@ -45,9 +54,32 @@ int main(int argc, char * argv[])
 	for (int i = argc_temp/2 + 1; i < argc_temp; i ++)
             parmList3[i - argc_temp/2] = argv[i];
 sleep(10);*/
-       //while (wait(&child_status) != pid)
-//	    ;
-  sleep(10);  }
+        waitpid(-1, &status, 0);
+
+        //pid = wait(&status);
+        //sleep(10);  
+    }
     return 0;
 }
 
+
+void child (int size, char * argv[])
+{
+	printf("Child %d is now running\n", getpid());
+	char ** parmList2;
+	parmList2 = (char **)calloc(size, sizeof(char*));
+        //char * parmList2[argc_temp/2 + 2];// = {argv[0]};
+	strcpy(parmList2[0],argv[0]);
+	for (int i = 1; i < size; i ++){
+	    parmList2[i] = malloc(sizeof(argv[i]));
+	    strcpy(parmList2[i],argv[i]);
+	    //parmList2[i] = argv[i];
+	}
+	//parmList2[argc_temp/2 + 1] = NULL;
+	printf("im a child :%s: :%s:\n", argv[0], argv[1]);	
+	printf("%d\n",size);
+        //exit(0);
+        sleep(1);
+	printf("im about to exec\n");
+	execvp(parmList2[0], parmList2);
+}
